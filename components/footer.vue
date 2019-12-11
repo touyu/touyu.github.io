a1298467fed5f8702e6136f8f1d731797950d11e
touyu <dev.akkey0222@gmail.com>
2019-12-12 01:16:57 +0900

    Add copyright

---
+++ components/footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="footer">
+    <p>Copyright © 2019 Yuto Akiba All Rights Reserved.</p>
+  </div>
+</template>
+
+<style scoped>
+.footer {
+  margin: 80px auto 80px auto;
+  text-align: center;
+  font-size: 11px;
+  color: #c2c2c2;
+}
+</style>
