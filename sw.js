0ababe70fd87874c8d4247bfc04408e7965ef38a
touyu <touyu@users.noreply.github.com>
2020-10-22 08:01:52 +0000

    deploy: e9fb406c8bd106b6bd7ea2ad2228eeff45062286

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d0887b539aa0cabbdb1f.js"
+    "url": "webpack-runtime-2c9901731c6c77307547.js"
   },
   {
     "url": "framework-dac95de3c301f57a114a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "18f77fea80eedfdcb82e4f35f67c190e"
+    "revision": "01a002663ee5c81d4da3de535478714a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
