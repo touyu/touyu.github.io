f4b3c356c3ac0b73d20fdc88ed585da538bce993
Yuto Akiba <dev.akkey0222@gmail.com>
2023-01-16 00:35:07 +0900

    Fix data

--- model/data.ts
+++ model/data.ts
@@ -1,10 +1,21 @@
 export const siteData = {
     title: "touyu.me",
-    sub_title: "Yuto Akiba",
+    subTitle: "Yuto Akiba",
     description: "Software Engineer",
-    background_image_path: "/touyu_many_computers_piles_miscellaneous_placed_deconstructed_d_59ecf491-e4ad-42be-821f-a19f3537f808.png",
-    prompt: `"many computers, piles, miscellaneous placed, deconstructed, deep sea"`,
-    generated_by: "Midjourney",
-    github_url: "https://github.com/touyu",
-    twitter_url: "https://twitter.com/akkey0222"
+    githubUrl: "https://github.com/touyu",
+    twitterUrl: "https://twitter.com/akkey0222"
 }
+
+export interface BackgroundImage {
+    path: string
+    prompt: string
+    generated_by: string
+}
+
+export const backgroundImages = [
+    {
+        path: "/touyu_many_computers_piles_miscellaneous_placed_deconstructed_d_59ecf491-e4ad-42be-821f-a19f3537f808.png",
+        prompt: `"many computers, piles, miscellaneous placed, deconstructed, deep sea`,
+        generatedBy: "Midjourney"
+    }
+]
