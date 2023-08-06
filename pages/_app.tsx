a24bba675a9f2d2eb98c218f6101c0ac0c1b690c
Yuto Akiba <dev.akkey0222@gmail.com>
2023-08-06 19:17:06 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {useEffect} from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.body.addEventListener('touchmove', function(e) {
+      e.preventDefault();
+    }, { passive: false });
+  }, []);
+
+
   return <Component {...pageProps} />
 }
